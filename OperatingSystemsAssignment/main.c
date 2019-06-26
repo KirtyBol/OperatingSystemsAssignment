e228702be49f3a246abb48a5de410fff9ed44da1
KirtyBol <0921444@hr.nl>
2019-06-26 22:08:44 +0200

    Initial Commit

---
+++ OperatingSystemsAssignment/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OperatingSystemsAssignment
+//
+//  Created by Kirty Bol on 26/06/2019.
+//  Copyright © 2019 Kirty Bol. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
